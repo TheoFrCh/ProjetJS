296a5bdeab611143a36fa8c47867132166f31dd1
Theo <theo.chiron26@gmail.fr>
2021-03-29 15:12:15 +0200

    Feat div that contain all the card and card style added #5

--- js/script.js
+++ js/script.js
@@ -10,10 +10,28 @@ $(document).ready(function () {
         .done(function (response) {//lorsque ma requête réussi 
             console.log(response) //affiche la response dans la console
             response.forEach(element => { //pour chaque element du tableau de mon api
-                const title = document.createElement("h3") //crée une variable titre qui sera un élément h3 dans le document
-                title.innerHTML = element.title //mon titre, avec la variable innerHTML va prendre pour valeur l'élément "title" de mon api 
-                //title.style.color = "green"; //je colore mon titre en vert
-                $("#feed").append(title) //replacer tout les éléments à l'interieur de mon id #feed par la balise h3 créée
+                const carte = document.createElement("div")
+                const titre = document.createElement("h3") //crée une variable titre qui sera un élément h3 dans le document
+                const desc = document.createElement("p")
+                const directeur = document.createElement("p")
+                const producteur = document.createElement("p")
+                const date = document.createElement("p")
+                const duree = document.createElement("p")
+                const note = document.createElement("p")
+
+                titre.innerHTML = element.title //mon titre, avec la variable innerHTML va prendre pour valeur l'élément "title" de mon api
+                desc.innerHTML = "Description : "+ element.description
+                directeur.innerHTML = "Directeur"+ element.director
+                producteur.innerHTML = "producteur"+ element.producer
+                date.innerHTML = "Date de sortie :"+ element.release_date
+                duree.innerHTML = "Durée : "+ element.running_time
+                note.innerHTML = "Score Rotten tomatoes : "+ element.rt_score
+                
+                carte.classList.add("carte")
+                carte.append(titre,desc,directeur,producteur,date,duree)
+
+                $("#feed").append(carte) //replacer tout les éléments à l'interieur de mon id #feed par la balise h3 créée
+                
             });
         })
 
